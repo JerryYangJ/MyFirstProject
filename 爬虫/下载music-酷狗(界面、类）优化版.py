3f4d2bf6970c80bf2c289e183c5ceef798172eac
JerryYangJ <113914359+JerryYangJ@users.noreply.github.com>
2023-07-10 13:48:40 +0800

    Update 下载music-酷狗(界面、类）优化版.py

--- 爬虫/下载music-酷狗(界面、类）优化版.py
+++ 爬虫/下载music-酷狗(界面、类）优化版.py
@@ -5,6 +5,9 @@
 
 '''
 谷歌webdriver下载地址：https://registry.npmmirror.com/binary.html?path=chromedriver/
+谷歌webdriver下载地址：https://registry.npmmirror.com/binary.html?path=chromedriver/
+放在python安装目录下
+
 酷狗音乐数据包地址：https://wwwapi.kugou.com/yy/index.php
     请求参数：
         data={
@@ -31,6 +34,8 @@
             ver: 0
             platform: web
 '''
+import time
+
 import requests
 from tkinter import *
 
@@ -39,10 +44,15 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 
 
+
 class main:
+
     def __init__(self):
         self.mu_name_list = [1, 2, 3, 4]
         self.num = 0
+
+        # 定义一个全局变量，记录是否为第一次下载
+        self.firstdownload = True
 
         self.win = Tk()
         self.win.title('酷狗音乐下载器')
@@ -91,30 +101,40 @@ class main:
 
     def download_music(self, num):
         print('你要下载的歌曲编号是：', num+1)
-        self.mu_name_list = self.web.find_elements(By.XPATH, '/html/body/div[4]/div[1]/div[2]/ul[2]/li/div[1]/a')
-        # print(mu_name_list)
+        if self.firstdownload:
+            self.mu_name_list = self.web.find_elements(By.XPATH, '/html/body/div[4]/div[1]/div[2]/ul[2]/li/div[1]/a')
+            print(self.mu_name_list)
+        print(self.mu_name_list)
 
         self.mu_name_list[num].click()
         music_name = self.mu_name_list[num].get_attribute('title')
 
         # 获取弹出的窗口
         win_list = self.web.window_handles
-        # print(win_list)
+        print(win_list)
         self.web.switch_to.window(win_list[-1])
 
         # 在弹出的播放页面源代码中找到下载地址
         url = self.web.find_element(By.XPATH, '/html/body/div[1]/audio').get_attribute('src')
-
         print(url)
+
+        # 关闭弹出的窗口，并将窗口句柄重新返回到所搜结果页
+        self.web.close()
+        print("弹窗已关闭")
+        self.web.switch_to.window(self.web.window_handles[-1])
 
         # 发送请求，下载数据
         headers = {
             "User-Agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.60 Safari/537.36 Edg/100.0.1185.29'
         }
         resp = requests.get(url, headers)
-        # print(resp)
+        print(resp)
         data = resp.content
         self.save_file(music_name, data)
+
+        self.firstdownload = False
+
+
 
     def save_file(self, music_name, data):
         # 保存数据
@@ -127,53 +147,3 @@ class main:
 if __name__ == '__main__':
     run = main()
     run.win.mainloop()
-
-
-'''
-music_name = input("请输入你要搜索的歌曲或歌手名称：")
-# 实例化浏览器，并访问酷狗
-# web = Chrome()
-
-# 无头参数设置
-opt = Options()
-opt.add_argument("--headless")  # 无头
-opt.add_argument('--disable-gpu')
-
-web = Chrome(options=opt)
-
-web.get(f'https://www.kugou.com/yy/html/search.html#searchType=song&searchKeyWord={music_name}')
-
-# 解析网页源代码
-mu_name_list = web.find_elements(By.XPATH, '/html/body/div[4]/div[1]/div[2]/ul[2]/li/div[1]/a')
-i = 1
-for elm in mu_name_list:
-    print(i, elm.get_attribute('title'))
-    i += 1
-
-num = int(input('请输入要下载的歌曲编号：'))-1
-mu_name_list[num].click()
-music_name = mu_name_list[num].get_attribute('title')
-
-# 获取弹出的窗口
-win_list = web.window_handles
-# print(win_list)
-web.switch_to.window(win_list[-1])
-
-# 在弹出的播放页面源代码中找到下载地址
-url = web.find_element(By.XPATH, '/html/body/div[1]/audio').get_attribute('src')
-
-print(url)
-
-# 发送请求，下载数据
-headers = {
-    "User-Agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.60 Safari/537.36 Edg/100.0.1185.29'
-}
-resp = requests.get(url, headers)
-# print(resp)
-data = resp.content
-
-# 保存数据
-with open(f'./music/{music_name}.mp3', 'wb') as f:
-    f.write(data)
-print(music_name, '下载完成')
-'''
